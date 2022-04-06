fcdf636cb5fe2774e5b199c0058bea3b299cc9f6
EloisaOliveira <eloisa.oliveirasantos@gmail.com>
2022-04-06 16:39:10 -0300

    :sparkles: feat: User Model

---
+++ src/model/User.ts
@@ -0,0 +1,21 @@
+import { v4 as uuidV4 } from 'uuid';
+
+class User {
+  id?: string;
+
+  name: string;
+
+  email: string;
+
+  created_at: Date;
+  
+  updated_at: Date;
+
+  constructor() {
+    if(!this.id) {
+      this.id = uuidV4();
+    }
+  }
+}
+
+export { User }
