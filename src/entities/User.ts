5dce4b5e1cd9e6864f32db66a964726b8fc07817
EloisaOliveira <eloisa.oliveirasantos@gmail.com>
2022-04-08 21:08:31 -0300

    :sparkles: feat: entidade user

---
+++ src/entities/User.ts
@@ -0,0 +1,25 @@
+import { v4 as uuidV4 } from 'uuid';
+import {Entity, PrimaryColumn, Column, CreateDateColumn } from 'typeorm';
+
+@Entity("user")
+class User {
+  @PrimaryColumn()
+  id?: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  constructor() {
+    if(!this.id) {
+      this.id = uuidV4();
+    }
+  }
+}
+
+export { User }
