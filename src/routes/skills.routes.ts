9ac1666fa25b63ffbcc9840a77909548fa7a3b74
alexsandro-cristiano <acg.alexsandro@hotmail.com>
2022-04-10 08:32:25 -0300

    aplicação respondendo a todas as rotas

--- src/routes/skills.routes.ts
+++ src/routes/skills.routes.ts
@@ -1,13 +1,26 @@
 import { Router } from 'express'
+import { SkillsController } from '../controllers/SkillsController'
 
 const skillsRoutes = Router()
+const skillsController = new SkillsController()
 
 skillsRoutes.get('/', (request, response) => {
-  return response.send('Entrei no verbo GET.')
+  skillsController.findAll(request, response)
+})
+
+skillsRoutes.get('/:id', (request, response) => {
+  skillsController.find(request, response)
 })
 
 skillsRoutes.post('/', (request, response) => {
-  return response.send('Entrei no verbo POST.')
+  skillsController.create(request, response)
 })
 
+skillsRoutes.put('/:id', (request, response) => {
+  skillsController.update(request, response)
+})
+
+skillsRoutes.delete('/:id', (request, response) => {
+  skillsController.delete(request, response)
+})
 export { skillsRoutes }
