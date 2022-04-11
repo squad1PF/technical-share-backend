f8e69a9e8b731c00ab20698ae65e335bad0ad746
alexsandro-cristiano <acg.alexsandro@hotmail.com>
2022-04-11 08:19:17 -0300

    :sparkles: feat: funcionalidade find construida

--- src/routes/skills.routes.ts
+++ src/routes/skills.routes.ts
@@ -1,13 +1,16 @@
 import { Router } from 'express'
 import { CreateSkillController } from '../controllers/CreateSkillController'
 import { FindAllSkillController } from '../controllers/FindAllSkillController'
+import { FindSkillController } from '../controllers/FindSkillController'
 
 const skillsRoutes = Router()
 
 const createSkillController = new CreateSkillController()
 const findAllSkillController = new FindAllSkillController()
+const findSkillController = new FindSkillController()
 
 skillsRoutes.post('/', createSkillController.handle)
 skillsRoutes.get('/', findAllSkillController.handle)
+skillsRoutes.get('/:id', findSkillController.handle)
 
 export { skillsRoutes }
